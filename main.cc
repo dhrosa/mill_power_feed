9f375a1173fee1858af4ba6f6e4aedbe5c94dac8
Diony Rosa <dhrosa@gmail.com>
2023-03-23 12:21:49 -0400

    Draw centered encoder text on OLED.

--- main.cc
+++ main.cc
@@ -1,14 +1,37 @@
-#include <pico/stdlib.h>
 #include <hardware/gpio.h>
 #include <hardware/i2c.h>
 #include <hardware/pwm.h>
+#include <pico/stdlib.h>
 
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <concepts>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include "rotary_encoder.h"
 #include "ssd1306.h"
 #include "textRenderer/TextRenderer.h"
-#include "rotary_encoder.h"
 
-#include <atomic>
-#include <iostream>
+void DrawCenteredValue(pico_ssd1306::SSD1306& display, int value) {
+  const unsigned char* font = font_16x32;
+  constexpr int line_width = 128;
+  constexpr int char_width = 16;
+  constexpr int chars_per_line = line_width / char_width;
+
+  std::array<char, chars_per_line + 1> buffer;
+  const int chars_written =
+      std::snprintf(buffer.data(), buffer.size(), "%+d", value);
+  const int left_padding = (chars_per_line - chars_written) / 2;
+
+  display.clear();
+  pico_ssd1306::drawText(&display, font, buffer.data(),
+                         left_padding * char_width, 0);
+  display.sendBuffer();
+}
 
 int main() {
   stdio_usb_init();
@@ -19,14 +42,12 @@ int main() {
     gpio_pull_up(pin);
   }
   pico_ssd1306::SSD1306 display(i2c0, 0x3C, pico_ssd1306::Size::W128xH32);
-  pico_ssd1306::drawText(&display, font_12x16, "test", 0, 0);
   display.sendBuffer();
 
   RotaryEncoder encoder(15, 25);
-  for (std::int64_t i = 0;; ++i) {
-    std::cout << i << ": " << encoder.Read() << std::endl;
+  while (true) {
+    DrawCenteredValue(display, encoder.Read());
     sleep_ms(100);
   }
-
   return 0;
 }
