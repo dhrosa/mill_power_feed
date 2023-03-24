dbafd50bf9d4e18200f572013a766935a840fa11
Diony Rosa <dhrosa@gmail.com>
2023-03-23 22:57:10 -0400

    RotaryEncoder now can track all transitions correctly by reading GPIO atomically.

--- rotary_encoder.cc
+++ rotary_encoder.cc
@@ -54,20 +54,16 @@ constexpr auto kIncrementsTable = IncrementsTable();
 
 void EdgeInterrupt() {
   const std::bitset<2> previous_values = values;
+  const std::bitset<32> all_gpio_values = gpio_get_all();
   for (int i : {0, 1}) {
-    const unsigned events = gpio_get_irq_event_mask(pins[i]);
-    if (events & GPIO_IRQ_EDGE_RISE) {
-      values[i] = true;
-    } else if (events & GPIO_IRQ_EDGE_FALL) {
-      values[i] = false;
-    }
     gpio_acknowledge_irq(pins[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL);
+    values[i] = all_gpio_values[pins[i]];
   }
   const unsigned transition =
       (previous_values.to_ulong() << 2) | values.to_ulong();
   fractional_counter += kIncrementsTable[transition];
   const int divisor = 4;
-  if (fractional_counter == -divisor || fractional_counter == +divisor) {
+  if (std::abs(fractional_counter) == divisor) {
     CriticalSectionLock lock(counter_critical_section);
     counter += fractional_counter / divisor;
     fractional_counter = 0;
@@ -80,13 +76,13 @@ RotaryEncoder::RotaryEncoder(unsigned pin_a, unsigned pin_b) {
   pins = {pin_a, pin_b};
   critical_section_init(&counter_critical_section);
 
-  irq_set_enabled(IO_IRQ_BANK0, true);
   for (unsigned pin : pins) {
     gpio_init(pin);
     gpio_pull_up(pin);
     gpio_add_raw_irq_handler(pin, EdgeInterrupt);
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
   }
+  irq_set_enabled(IO_IRQ_BANK0, true);
 }
 
 std::int64_t RotaryEncoder::Read() {
