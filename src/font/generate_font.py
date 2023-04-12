28e6d23aaad8a9398d87a4a6ea22a7981b5dd1c7
Diony Rosa <dhrosa@gmail.com>
2023-04-12 13:48:07 -0400

    Utility for generating font data using the Spleen font.
    Font source: https://github.com/fcambus/spleen

---
+++ src/font/generate_font.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from collections import namedtuple
+from math import ceil
+from itertools import product
+import numpy as np
+
+Font = namedtuple("Font", ("char_data", "width", "height"))
+
+
+def load_font_data(image_path):
+    from PIL import Image
+
+    with Image.open(image_path) as image:
+        # Convert to bitmap
+        image.convert("1")
+        image_width = image.width
+        height = image.height
+        # Numpy array of 0's and 1's
+        data = np.array(image.getdata()).reshape(height, image_width)
+
+    char_count = 95
+    width = image_width // char_count
+    # Array indexed by (char_num, row, col)
+    char_data = np.array(np.hsplit(data, char_count))
+
+    return Font(char_data, width, height)
+
+
+def char_to_blocks(font, i):
+    width = font.width
+    # Each image is split up into blocks where each block is a byte of 8 contiguous
+    # pixels vertically, where the LSB is the top-most pixel.
+
+    # Pad char vertically so each column is a multiple of 8 pixels.
+    blocks_per_column = ceil(font.height / 8)
+    storage_height = 8 * blocks_per_column
+    pixels = font.char_data[i].copy()
+    pixels.resize(storage_height, font.width)
+
+    # Pack bits over the row axis. Resulting shape will be (blocks_per_column,
+    # width).
+    packed = np.packbits(pixels, axis=0, bitorder="little")
+    return packed.tobytes()
+
+
+def main():
+    from argparse import ArgumentParser
+    from sys import stdout
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "image_path",
+        type=Path,
+        help="Path to image of monospace font consisting of the horizontal concatenation of the ASCII range [' ', '~'].",
+    )
+    parser.add_argument("output_path", type=Path, help="Path to write block blob to.")
+
+    args = parser.parse_args()
+    font = load_font_data(args.image_path)
+    with args.output_path.open("wb") as output:
+        for i in range(len(font.char_data)):
+            output.write(char_to_blocks(font, i))
+
+
+if __name__ == "__main__":
+    main()
