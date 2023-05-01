e03609ab461d0d5f0b5dbfdc148d700ce9bb28ec
Diony Rosa <dhrosa@gmail.com>
2023-05-01 14:09:34 -0400

    Add co_await support to Task

--- src/picoro/task.h
+++ src/picoro/task.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <coroutine>
+#include <cstdint>
 
-#include "picoro/notification.h"
+#include "picopp/critical_section.h"
 
 // Simple coroutine with no return value. Execution is eager: the body of the
 // coroutine will start executing immediately and continue until the first
@@ -16,8 +17,9 @@ class Task {
 
   ~Task();
 
-  // Blocks until the coroutine has finished execution.
-  void Wait();
+  // Awaitable that suspends the current coroutine until this task has
+  // completed.
+  auto operator co_await();
 
  private:
   Task(std::coroutine_handle<promise_type> handle_) : handle_(handle_) {}
@@ -26,7 +28,9 @@ class Task {
 };
 
 struct Task::promise_type {
-  Notification complete;
+  CriticalSection mutex;
+  bool done = false;
+  std::coroutine_handle<> waiter;
 
   Task get_return_object() {
     return {std::coroutine_handle<promise_type>::from_promise(*this)};
@@ -35,12 +39,20 @@ struct Task::promise_type {
   std::suspend_never initial_suspend() { return {}; }
 
   auto final_suspend() noexcept {
-    struct Awaiter : std::suspend_always {
-      Notification& complete;
-      void await_suspend(std::coroutine_handle<>) { complete.Notify(); }
+    struct FinalSuspend : std::suspend_always {
+      promise_type& promise;
+      std::coroutine_handle<> await_suspend(std::coroutine_handle<> handle) {
+        CriticalSectionLock lock(promise.mutex);
+        promise.done = true;
+        if (promise.waiter) {
+          return promise.waiter;
+        }
+        return std::noop_coroutine();
+      }
     };
-    return Awaiter{.complete = complete};
-  };
+
+    return FinalSuspend{.promise = *this};
+  }
   void return_void() {}
 };
 
@@ -57,9 +69,26 @@ inline Task::~Task() {
   }
 }
 
-inline void Task::Wait() {
-  if (!handle_) {
-    return;
-  }
-  handle_.promise().complete.Wait();
+inline auto Task::operator co_await() {
+  struct Awaiter {
+    promise_type& promise;
+
+    bool await_ready() {
+      CriticalSectionLock lock(promise.mutex);
+      return promise.done;
+    }
+
+    bool await_suspend(std::coroutine_handle<> handle) {
+      CriticalSectionLock lock(promise.mutex);
+      if (promise.done) {
+        return false;
+      }
+      promise.waiter = handle;
+      return true;
+    }
+
+    void await_resume() {}
+  };
+
+  return Awaiter{.promise = handle_.promise()};
 }
