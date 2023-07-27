eac838d3fecb751b7e6e0bc169fd7be14a422392
Diony Rosa <dhrosa@gmail.com>
2023-07-27 01:28:18 -0400

    Add decimal, binary, and 16-bit hex views of captures Prolific data.

--- analysis/prolific.py
+++ analysis/prolific.py
@@ -8,20 +8,26 @@ def keep_field(proto_name, name):
     if proto_name == "fake-field-wrapper" and parts[1] == "capdata":
         return parts[1]
     if proto_name == "usb":
-        if parts[1] == "endpoint_address":
-            return "endpoint" if parts[2] == "number" else None
         if parts[1] in (
             "transfer_type",
-            "urb_status",
-            "request_in",
+            "src",
         ):
             return parts[1]
     if proto_name == "frame":
         if parts[1] == "time_relative":
             return "t"
-        if parts[1] in ("number", "interface_name"):
-            return parts[1]
+        if parts[1] == "number":
+            return "frame_number"
     return None
+
+
+def field_value(field):
+    value = field.get("value")
+    if value is None:
+        return field.get("show")
+    if len(value) == 1:
+        value = b"0" + value
+    return unhexlify(value)
 
 
 def packet_to_record(packet):
@@ -32,81 +38,51 @@ def packet_to_record(packet):
             name = keep_field(proto_name, field.get("name"))
             if name is None:
                 continue
-            if value := field.get("value"):
-                if len(value) == 1:
-                    value = "0" + value
-                value = unhexlify(value)
-                if name != "capdata":
-                    value = int.from_bytes(
-                        value, byteorder="little", signed=name == "urb_status"
-                    )
-            else:
-                value = field.get("show")
-            columns[name] = value
+            columns[name] = field_value(field)
     return columns
 
 
-def packets():
+def bulk_packets():
     from lxml import etree
-    from errno import errorcode
 
-    tree = etree.parse("prolific.pdml")
-    root = tree.getroot()
-
+    root = etree.parse("prolific.pdml").getroot()
     df = DataFrame.from_records(packet_to_record(p) for p in root)
-    # Filter to only single interface.
-    df = df[df.interface_name == "usbmon0"].drop(columns="interface_name")
-    # Map urb_status errno values.
-    df = df.assign(
-        urb_status=df.urb_status.map(
-            lambda e: "eok" if e == 0 else errorcode.get(-e).lower()
-        )
+
+    # Drop packets with no data.
+    df = df.dropna()
+
+    # Map transfer types and filter to bulk packets.
+    bulk = (
+        df.pop("transfer_type")
+        .map(int.from_bytes)
+        .map({0: "isochronous", 1: "interrupt", 2: "control", 3: "bulk"})
+        .eq("bulk")
     )
-    # Map transfer types
-    df = df.assign(
-        transfer_type=df.transfer_type.map(
-            {0: "isochronous", 1: "interrupt", 2: "control", 3: "bulk"}
-        )
-    )
-    # Shift frame number to index.
-    return df.set_index("number")
+    df = df[bulk]
+
+    # Boolean host label.
+    df["host"] = df.pop("src").eq("host")
+
+    # Each host->device transfer marks a new request.
+    df["request_number"] = df.host.cumsum() - 1
+
+    # Explode to single byte per row.
+    df.capdata = df.capdata.map(list)
+    df = df.explode("capdata")
+
+    # Label each byte within each frame.
+    df["byte_number"] = df.groupby(["request_number", "host"]).request_number.cumcount()
+
+    df["data"] = df.pop("capdata")
+    df["data_hex"] = df.data.map("{:02X}".format)
+    df["data_bin"] = df.data.map("{:08b}".format)
+
+    # 16-bit interpretation of byte pairs
+    data16_lsb = df.groupby(["request_number", "host"]).data.shift(-1)
+    df["data16"] = (df.data * 256) + data16_lsb
+    return df.reset_index(drop=True)
 
 
-df = packets()
-
-requests = df[df.request_in.isna()].drop(columns=["request_in", "urb_status"])
-responses = df[df.request_in.notna()].set_index("request_in")[["urb_status", "capdata"]]
-convos = requests.join(responses, lsuffix="_host", rsuffix="_device")
-transfers = convos[convos.transfer_type == "bulk"].drop(
-    columns=["transfer_type", "endpoint", "urb_status"]
-)
-
-transfers = transfers.fillna({"capdata_device": bytes()})
-
-# Unique index that increments each time there's a host->device transfer.
-transfers = transfers.assign(transfer_index=transfers.capdata_host.notna().cumsum())
-
-# Aggregate device->host payloads together per transfer.
-transfers = (
-    transfers.groupby("transfer_index")
-    .agg(
-        {"t": "first", "capdata_host": "first", "capdata_device": lambda x: b"".join(x)}
-    )
-    .tail(-1)
-)
-
-
-def pretty_hex(bs):
-    return bs.hex(" ")
-
-
-print(
-    pd.concat(
-        dict(
-            t=transfers.t,
-            host=transfers.capdata_host.map(pretty_hex),
-            device=transfers.capdata_device.map(pretty_hex),
-        ),
-        axis=1,
-    ).to_string(justify="left", index=False)
-)
+pd.set_option("display.max_rows", 0)
+df = bulk_packets()
+print(df)
