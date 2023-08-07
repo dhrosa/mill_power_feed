859b3e94fd67b781d81d273fe466d80479306a6a
Diony Rosa <dhrosa@gmail.com>
2023-08-07 15:48:23 -0400

    Use adafruit_display_text to render text, in preparation for more complex UI

--- motor_programmer/code.py
+++ motor_programmer/code.py
@@ -4,28 +4,82 @@ from busio import UART
 import microcontroller
 from parameters import Parameters
 from fake_stream import FakeStream
+import displayio
+from adafruit_display_text import label, bitmap_label
+import terminalio
 
-print()
+print("\nStartup")
+
+
+def make_label(*args, **kwargs):
+    kwargs.update({"font": terminalio.FONT})
+    return label.Label(*args, **kwargs)
+
+
+def translate(pos, delta):
+    return (pos[0] + delta[0], pos[1] + delta[1])
 
 
 class Ui:
     def __init__(self, params, macropad):
         self._params = params
         self._macropad = macropad
-        self._text = macropad.display_text(title="Parameter Editor")
         self._page_index = 0
         self._offset = 0
+
+        width = macropad.display.width
+        height = macropad.display.height
+
+        self._root = displayio.Group()
+
+        title = make_label(text="Parameter Editor")
+        self._root.append(title)
+        title.anchor_point = (0.5, 0)
+        title.anchored_position = (width / 2, 0)
+
+        form = displayio.Group(y=title.anchored_position[1] + title.height)
+        self._root.append(form)
+
+        key_label = make_label(text="Key: ")
+        form.append(key_label)
+        key_label.anchor_point = (1, 0)
+        key_label.anchored_position = (width / 2, 0)
+
+        value_label = make_label(text="Value: ")
+        form.append(value_label)
+        value_label.anchor_point = key_label.anchor_point
+        value_label.anchored_position = translate(
+            key_label.anchored_position, (0, key_label.height)
+        )
+
+        key = make_label(text="Pr12.34")
+        form.append(key)
+        key.anchor_point = (0, 0)
+        key.anchored_position = (width / 2, 0)
+        self._key_text = key
+
+        value = make_label(text="12345")
+        form.append(value)
+        value.anchor_point = (0, 0)
+        value.anchored_position = translate(key.anchored_position, (0, key.height))
+        self._value_text = value
+
+        self._update_key_value_text()
 
     @property
     def page(self):
         return self._params.pages[self._page_index]
 
-    def render(self):
+    def _update_key_value_text(self):
         page = self.page
         key = self.page.absolute_key(self._offset)
-        self._text[0].text = f"  Key: {page.number}.{self._offset:02d} ({key:3d})"
-        self._text[1].text = f"Value: {page[self._offset]:6d}"
-        self._text.show()
+        offset = self._offset
+
+        self._key_text.text = f"{page.number}.{offset:02d} ({key:3d})"
+        self._value_text.text = f"{page[offset]:6d}"
+
+    def render(self):
+        self._macropad.display.root_group = self._root
 
     def advance(self, page_delta=0, offset_delta=0):
         if offset_delta:
@@ -37,7 +91,9 @@ class Ui:
             self._offset = 0
             self._page_index += page_delta
             self._page_index %= len(self._params.pages)
+        self._update_key_value_text()
         self.render()
+
 
 # Pinout:
 # https://cdn-learn.adafruit.com/assets/assets/000/104/022/original/adafruit_products_Adafruit_MacroPad_RP2040_Pinout.png?1629726427
@@ -48,7 +104,7 @@ params = Parameters(modbus_stream)
 
 
 macropad = MacroPad()
-text_lines = macropad.display_text(title="Parameter Editor")
+# text_lines = macropad.display_text(title="Parameter Editor")
 
 ui = Ui(params, macropad)
 ui.render()
