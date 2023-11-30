5091a97d29cee494abe38b4e96a257df7a6e4758
Diony Rosa <dhrosa@gmail.com>
2023-11-30 13:43:46 -0500

    Cleanup push.py and rename 'push' command to 'upload'.

--- push.py
+++ push.py
@@ -5,6 +5,7 @@ import json
 from pathlib import Path
 import shutil
 from sys import exit
+from os import execlp
 
 from types import SimpleNamespace
 from pprint import pprint
@@ -13,6 +14,7 @@ from dataclasses import dataclass, asdict
 
 
 def run(args):
+    """Execute command and return its stdout output."""
     process = subprocess.run(args, capture_output=True, text=True)
     try:
         process.check_returncode()
@@ -28,15 +30,25 @@ def run(args):
 
 @dataclass
 class Device:
+    """A CircuitPython composite USB device."""
+
     vendor: str
     model: str
     serial: str
 
+    # Path to partition device.
     partition_path: str = None
+
+    # Path to serial device.
     serial_path: str = None
 
 
 def get_device_info(path):
+    """
+    Extract device attributes from udevadm.
+
+    Returns None if the device is not a USB device.
+    """
     info = {}
     args = f"udevadm info --query=property --name {path}".split()
     for line in run(args).splitlines():
@@ -48,6 +60,8 @@ def get_device_info(path):
 
 
 def all_devices():
+    """Finds all USB CircuitPython devices."""
+
     devices = []
 
     def find_or_add_device(info):
@@ -66,6 +80,7 @@ def all_devices():
         devices.append(device)
         return device
 
+    # Find CIRCUITPY partition devices.
     for path in Path("/dev/disk/by-id/").iterdir():
         info = get_device_info(path)
         if (
@@ -77,6 +92,7 @@ def all_devices():
         device = find_or_add_device(info)
         device.partition_path = str(path)
 
+    # Find serial devices.
     for path in Path("/dev/serial/by-id/").iterdir():
         info = get_device_info(path)
         if info is None:
@@ -88,14 +104,15 @@ def all_devices():
 
 
 def get_mountpoint(partition_path):
+    """Find mountpoint of given partition device. Returns empty string if not mounted."""
     args = f"lsblk {partition_path} --output mountpoint --noheadings".split()
     return run(args).strip()
 
 
 def mount_if_needed(partition_path):
+    """Mounts the given partition device if needed, and returns the mountpoint."""
     mountpoint = get_mountpoint(partition_path)
     if mountpoint:
-        print(f"Device already mounted at {mountpoint}.")
         return mountpoint
     mount_stdout = run(
         f"udisksctl mount --block-device {partition_path} --options noatime".split()
@@ -119,7 +136,8 @@ def unique_device(devices):
     return devices[0]
 
 
-def push_tree(source_dir, dest_dir):
+def copy_tree(source_dir, dest_dir):
+    """Copy contents of source directory into destination directory."""
     for source in source_dir.rglob("*"):
         if source.name[0] == "." or source.is_dir():
             # print(f'Skipping {source}')
@@ -137,17 +155,62 @@ def push_tree(source_dir, dest_dir):
         shutil.copy2(source, dest)
 
 
+def list_command(devices):
+    """list subcommand."""
+    print("Matching devices:")
+    pprint(devices)
+
+
+def upload_command(device, source_dirs):
+    """upload subcommand."""
+    print("Uploading to device: ")
+    pprint(device)
+    for source_dir in source_dirs:
+        if not source_dir.exists():
+            exit(f"Path '{source_dir}' does not exist.")
+        if not source_dir.is_dir():
+            exit(f"Path '{source_dir}' is not a directory.")
+
+    dest_dir = Path(mount_if_needed(device.partition_path))
+
+    for source_dir in source_dirs:
+        copy_tree(source_dir, dest_dir)
+    print("Upload complete.")
+
+
+def serial_command(device):
+    """serial subcommand."""
+    print("Launching minicom for ")
+    pprint(device)
+    execlp("minicom", "minicom", "-D", device.serial_path)
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--vendor", type=str, default="")
-    parser.add_argument("--model", type=str, default="")
-    parser.add_argument("--serial", type=str, default="")
+    parser.add_argument(
+        "--vendor",
+        type=str,
+        default="",
+        help="Filter to devices whose vendor contains this string.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="",
+        help="Filter to devices whose model contains this string.",
+    )
+    parser.add_argument(
+        "--serial",
+        type=str,
+        default="",
+        help="Filter to devices whose serial contains this string.",
+    )
 
     subparsers = parser.add_subparsers(required=True, dest="command")
 
     subparsers.add_parser("list")
-    push_parser = subparsers.add_parser("push")
-    push_parser.add_argument("source_dir", type=Path, nargs="+")
+    upload_parser = subparsers.add_parser("upload")
+    upload_parser.add_argument("source_dir", type=Path, nargs="+")
     subparsers.add_parser("serial")
 
     args = parser.parse_args()
@@ -163,33 +226,14 @@ def main():
         )
 
     devices = [d for d in all_devices() if device_filter(d)]
-    if args.command == "list":
-        print("Matching devices:")
-        pprint(devices)
-        exit()
 
-    device = unique_device(devices)
-    print("Selected device:")
-    pprint(device)
-
-    if args.command == "serial":
-        import os
-
-        os.execlp("minicom", "minicom", "-D", device.serial_path)
-
-    assert args.command == "push"
-
-    for source_dir in args.source_dir:
-        if not source_dir.exists():
-            exit(f"Path '{source_dir}' does not exist.")
-        if not source_dir.is_dir():
-            exit(f"Path '{source_dir}' is not a directory.")
-
-    dest_dir = Path(mount_if_needed(device.partition_path))
-
-    for source_dir in args.source_dir:
-        push_tree(source_dir, dest_dir)
-    print("Push complete.")
+    match args.command:
+        case "list":
+            list_command(devices)
+        case "upload":
+            upload_command(unique_device(devices), args.source_dir)
+        case "serial":
+            serial_command(unique_device(devices))
 
 
 if __name__ == "__main__":
